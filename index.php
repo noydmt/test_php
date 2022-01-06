5802d5f70fea0ecbd3d397dfa55aa580788ff98f
NaoyaTsudome <noy.dmt1021@gmail.com>
2022-01-07 00:59:19 +0900

    配列定義、要素取り出し、全要素確認

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
-const TEST = '124';
-const TEST = '222';
-echo TEST; // => 124
+$array = [1,2,3];
+echo $array; // => Array
+echo $array[0]; // => 1
+var_dump($array); // => array(3) { [0]=> int(1) [1]=> int(2) [2]=> int(3) }
+?>
