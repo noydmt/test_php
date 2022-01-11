35ec7b7336c1e36f486fd2943accbfef794fd0b9
NaoyaTsudome <noy.dmt1021@gmail.com>
2022-01-12 00:11:02 +0900

    elseを使用したif分岐

--- index.php
+++ index.php
@@ -1,32 +1,11 @@
 <?php
-$japan_soccer_national_team = [
-  'honda'=>[
-    'position'=>'FW',
-    'club'=>'AC Milan'
-  ],
-  'kagawa'=>[
-    'position'=>'MF',
-    'club'=>'Manchester United'
-  ],
-  'nagatomo'=>[
-    'position'=>'SB',
-    'club'=>'Intel'
-  ]
-];
+$signal = '赤';
 
-if ($japan_soccer_national_team['honda']['position'] == 'FW') {
-  echo '本田はFWだ'; // 出力される
-};
-
-echo '<br />';
-
-if ($japan_soccer_national_team['kagawa']['club'] === 'Manchester United') {
-  echo '香川は' . $japan_soccer_national_team['kagawa']['club'] . '所属だ!'; // 出力される。
-};
-
-/**
- * if 分岐で 比較演算子が下記の場合、注意する
- * 1, == : 値が一致していれば True
- * 2, === : 値と型が一致していれば True
- */
+if ($signal === '青') {
+  echo '進め。';
+} else if ($signal === '黄') {
+  echo '気をつけて進め。';
+} else if ($signal === '赤') {
+  echo '止まれ。';
+}
 ?>
