c92fa551645115e0ce116451c2f8f013afdc0815
NaoyaTsudome <noy.dmt1021@gmail.com>
2022-01-12 00:03:50 +0900

    if文で使用する比較演算子=の数による違い

--- index.php
+++ index.php
@@ -16,5 +16,17 @@ $japan_soccer_national_team = [
 
 if ($japan_soccer_national_team['honda']['position'] == 'FW') {
   echo '本田はFWだ'; // 出力される
-}
+};
+
+echo '<br />';
+
+if ($japan_soccer_national_team['kagawa']['club'] === 'Manchester United') {
+  echo '香川は' . $japan_soccer_national_team['kagawa']['club'] . '所属だ!'; // 出力される。
+};
+
+/**
+ * if 分岐で 比較演算子が下記の場合、注意する
+ * 1, == : 値が一致していれば True
+ * 2, === : 値と型が一致していれば True
+ */
 ?>
