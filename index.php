e5415a4d6590a57c85d4307d7d8a970b0223d6bd
NaoyaTsudome <noy.dmt1021@gmail.com>
2022-01-13 23:39:17 +0900

    値が空か精査する enpty メソッド

--- index.php
+++ index.php
@@ -1,11 +1,7 @@
 <?php
-$signal = '赤';
+$signal = '';
 
-if ($signal === '青') {
-  echo '進め。';
-} else if ($signal === '黄') {
-  echo '気をつけて進め。';
-} else if ($signal === '赤') {
-  echo '止まれ。';
+if (empty($signal)) {
+  echo '進め。'; // 出力される
 }
 ?>
