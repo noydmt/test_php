83ac1cae24a6a2451feac077db67eef75fd2c6a7
NaoyaTsudome <noy.dmt1021@gmail.com>
2022-01-15 13:49:31 +0900

    関数定義

--- index.php
+++ index.php
@@ -1,23 +1,7 @@
 <?php
-/**
- * 注意！
- * Switch文の場合は比較する際に == で比較している為、型が違くても true判定してしまうときがある。　　
- */
-
-$cnt = 1;
-switch($cnt) {
-  case 1:
-    echo '1です';
-    break;
-  case 2:
-    echo '10です';
-    break;
-  case 3:
-    echo '11です';
-    break;
-  default:
-    echo 'その他';
-    break;
+function test () {
+  echo 'テスト';
 }
 
+test();
 ?>
