6b9eb13baa77a19fd7efd4391331075a9eebe08a
NaoyaTsudome <noy.dmt1021@gmail.com>
2022-01-15 13:18:00 +0900

    基本的なbreakとcontinue文

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php
-$arrayList = array(1,2,3,4,5,6);
+$arrayList = [1,2,3,4,5,6];
 
 for ($i = 0; $i < count($arrayList); $i++) {
-  echo $arrayList[$i];
+  if ($arrayList[$i] === 5) {
+    echo '終了!';
+    break;
+  } else {
+    echo $arrayList[$i] . 'です。';
+    continue;
+  }
 }
 
 ?>
