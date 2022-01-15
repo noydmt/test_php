ec42db71bd40f7f4c8ecc37bf89a6d42093b092a
NaoyaTsudome <noy.dmt1021@gmail.com>
2022-01-15 13:42:34 +0900

    swith文 使用に注意

--- index.php
+++ index.php
@@ -1,10 +1,23 @@
 <?php
-$arrayList = [1,2,3,4,5,6];
-$cnt = 0;
-while($cnt < 6) {
-  echo $cnt;
-  $cnt++;
-  echo '<br />';
+/**
+ * 注意！
+ * Switch文の場合は比較する際に == で比較している為、型が違くても true判定してしまうときがある。　　
+ */
+
+$cnt = 1;
+switch($cnt) {
+  case 1:
+    echo '1です';
+    break;
+  case 2:
+    echo '10です';
+    break;
+  case 3:
+    echo '11です';
+    break;
+  default:
+    echo 'その他';
+    break;
 }
 
 ?>
