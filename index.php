7707fcda8b1b2cb4eec51f696a1dd84520629b82
NaoyaTsudome <noy.dmt1021@gmail.com>
2022-01-15 13:21:50 +0900

    基本的なWhile文

--- index.php
+++ index.php
@@ -1,14 +1,10 @@
 <?php
 $arrayList = [1,2,3,4,5,6];
-
-for ($i = 0; $i < count($arrayList); $i++) {
-  if ($arrayList[$i] === 5) {
-    echo '終了!';
-    break;
-  } else {
-    echo $arrayList[$i] . 'です。';
-    continue;
-  }
+$cnt = 0;
+while($cnt < 6) {
+  echo $cnt;
+  $cnt++;
+  echo '<br />';
 }
 
 ?>
