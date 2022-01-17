9e64dc1ef3a80e0677df3441b90c625b81f250d4
NaoyaTsudome <noy.dmt1021@gmail.com>
2022-01-18 00:07:59 +0900

    mb_strlen関数

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
 
 // 文字列の長さを調べる
-$str = 'ggsgsdgsdgsd';
-echo strlen($str);
+$str = 'あいう';
+echo strlen($str); // => 9
+echo '<br>';
+echo mb_strlen($str); // => 3
+
+// 日本語の場合、一文字あたり 3byte必要とする
+// mb_strlen の場合、純粋に文字列の長さでみる。
 
 ?>
