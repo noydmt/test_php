976f57f8964666ae06999120a4bfeb148fda0213
NaoyaTsudome <noy.dmt1021@gmail.com>
2022-01-18 00:21:58 +0900

    正規表現

--- index.php
+++ index.php
@@ -1,12 +1,7 @@
 <?php
 
-// 文字列の分割
-$str = 'aaa,aaaa,a,bb,b';
-$str2 = explode(',',$str);
-echo explode(',',$str); // => Array
-echo '<br>';
-echo $str2[1]; // => aaaa
-echo '<br>';
-echo $str2[3]; // => bb
+// 正規表現
+$str = 'テスト終了';
+echo preg_match('/テスト/',$str); // => 1
 
 ?>
