bc1a46a2ff7f3254daa067419f165365477041a7
NaoyaTsudome <noy.dmt1021@gmail.com>
2022-01-18 00:18:16 +0900

    文字列の分割

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php
 
-// 文字列の置き換え
-$str = 'aaaaaaaabbb';
-
-echo str_replace('aaa','ccc',$str); // => ccccccaabbb
+// 文字列の分割
+$str = 'aaa,aaaa,a,bb,b';
+$str2 = explode(',',$str);
+echo explode(',',$str); // => Array
+echo '<br>';
+echo $str2[1]; // => aaaa
+echo '<br>';
+echo $str2[3]; // => bb
 
 ?>
