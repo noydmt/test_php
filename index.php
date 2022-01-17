ce808d50c3ed994844f6d187a132fa6c202a674a
NaoyaTsudome <noy.dmt1021@gmail.com>
2022-01-18 00:30:27 +0900

    配列に要素追加

--- index.php
+++ index.php
@@ -1,7 +1,21 @@
 <?php
 
 // 正規表現
-$str = 'テスト終了';
-echo preg_match('/テスト/',$str); // => 1
-
+$strArray = ['A','B','C'];
+array_push($strArray,'D','E');
+echo '<pre>';
+var_dump($strArray);
+echo '</ pre>';
+// array(5) {
+//   [0]=>
+//   string(1) "A"
+//   [1]=>
+//   string(1) "B"
+//   [2]=>
+//   string(1) "C"
+//   [3]=>
+//   string(1) "D"
+//   [4]=>
+//   string(1) "E"
+// }
 ?>
