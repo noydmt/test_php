5146f42e2625765a3bb406ab11c172fdc3e9a7e1
NaoyaTsudome <noy.dmt1021@gmail.com>
2022-01-18 22:34:28 +0900

    グローバル変数をメソッド内で使用する

--- index.php
+++ index.php
@@ -1,21 +1,11 @@
 <?php
+$global = 'グローバル変数';
 
-// 正規表現
-$strArray = ['A','B','C'];
-array_push($strArray,'D','E');
-echo '<pre>';
-var_dump($strArray);
-echo '</ pre>';
-// array(5) {
-//   [0]=>
-//   string(1) "A"
-//   [1]=>
-//   string(1) "B"
-//   [2]=>
-//   string(1) "C"
-//   [3]=>
-//   string(1) "D"
-//   [4]=>
-//   string(1) "E"
-// }
+function checkTest() {
+  $local = 'ローカル変数';
+  global $global;
+  echo $global;
+}
+
+checkTest();
 ?>
