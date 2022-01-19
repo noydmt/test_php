a191986574093af7015e4369802c09cb74283141
NaoyaTsudome <noy.dmt1021@gmail.com>
2022-01-19 23:52:55 +0900

    戻るボタンの追加実装

--- form/input.php
+++ form/input.php
@@ -1,4 +1,5 @@
 <?php
+// var_dump($_POST);
 $pageflg = 0;
 if (!empty($_POST['btn_confirm'])) {
   $pageflg = 1;
@@ -20,21 +21,22 @@ if (!empty($_POST['btn_submit'])) {
   <?php if ($pageflg === 0) : ?>
     <form method="POST" action="input.php">
       <p>氏名</p>
-      <input type="text" name="your_name">
+      <input type="text" name="your_name" value="<?php if(!empty($_POST['your_name'] )) { echo $_POST['your_name']; }?>">
       <p>e-mail</p>
-      <input type="email" name="your_email">
+      <input type="email" name="your_email" value="<?php if (!empty($_POST['your_email'])) { echo $_POST['your_email']; }?>">
       <input type="submit" name="btn_confirm" value="確認">
     </form>
   <?php endif; ?>
   <?php if ($pageflg === 1) : ?>
     <form method="POST" action="input.php">
       <p>氏名</p>
-      <?php echo $_POST['your_name'] ?>
+      <?php echo $_POST['your_name']; ?>
       <p>e-mail</p>
-      <?php echo $_POST['your_email'] ?>
+      <?php echo $_POST['your_email']; ?>
+      <input type="submit" name="back" value="戻る">
       <input type="submit" name="btn_submit" value="送信">
-      <input type="hidden" names="your_name" value="<?php echo $_POST['your_name'] ?>">
-      <input type="hidden" names="your_email" value="<?php echo $_POST['your_email'] ?>">
+      <input type="hidden" name="your_name" value="<?php echo $_POST['your_name']; ?>">
+      <input type="hidden" name="your_email" value="<?php echo $_POST['your_email']; ?>">
     </form>
   <?php endif; ?>
   <?php if ($pageflg === 2) : ?>
