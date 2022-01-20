18a2fb1de5a85eeb5385b75ddd0666ea3600f173
NaoyaTsudome <noy.dmt1021@gmail.com>
2022-01-20 23:05:24 +0900

    クロスサイトスクリプティング対策

--- form/input.php
+++ form/input.php
@@ -8,6 +8,10 @@ if (!empty($_POST['btn_submit'])) {
   $pageflg = 2;
 }
 // $_GET => スーパーグローバル変数。中身は連想配列になっている。
+
+function h(string $str) {
+  return htmlspecialchars($str, ENT_QUOTES,'UTF-8');
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -19,24 +23,25 @@ if (!empty($_POST['btn_submit'])) {
 </head>
 <body>
   <?php if ($pageflg === 0) : ?>
+  <?php $csrfToken = bin2hex(random_bytes(32)); ?>
     <form method="POST" action="input.php">
       <p>氏名</p>
-      <input type="text" name="your_name" value="<?php if(!empty($_POST['your_name'] )) { echo $_POST['your_name']; }?>">
+      <input type="text" name="your_name" value="<?php if(!empty($_POST['your_name'] )) { echo h($_POST['your_name']); }?>">
       <p>e-mail</p>
-      <input type="email" name="your_email" value="<?php if (!empty($_POST['your_email'])) { echo $_POST['your_email']; }?>">
+      <input type="email" name="your_email" value="<?php if (!empty($_POST['your_email'])) { echo h($_POST['your_email']); }?>">
       <input type="submit" name="btn_confirm" value="確認">
     </form>
   <?php endif; ?>
   <?php if ($pageflg === 1) : ?>
     <form method="POST" action="input.php">
       <p>氏名</p>
-      <?php echo $_POST['your_name']; ?>
+      <?php echo h($_POST['your_name']); ?>
       <p>e-mail</p>
-      <?php echo $_POST['your_email']; ?>
+      <?php echo h($_POST['your_email']); ?>
       <input type="submit" name="back" value="戻る">
       <input type="submit" name="btn_submit" value="送信">
-      <input type="hidden" name="your_name" value="<?php echo $_POST['your_name']; ?>">
-      <input type="hidden" name="your_email" value="<?php echo $_POST['your_email']; ?>">
+      <input type="hidden" name="your_name" value="<?php echo h($_POST['your_name']); ?>">
+      <input type="hidden" name="your_email" value="<?php echo h($_POST['your_email']); ?>">
     </form>
   <?php endif; ?>
   <?php if ($pageflg === 2) : ?>
