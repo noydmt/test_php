45db6a1615be122252c4060778c140506ccc7d29
NaoyaTsudome <noy.dmt1021@gmail.com>
2022-01-23 16:24:37 +0900

    メールアドレスのバリデーション

--- form/validation.php
+++ form/validation.php
@@ -2,8 +2,20 @@
   function validation($request) {
     $errors = [];
 
+    // 氏名バリデーション
     if (empty($request['your_name'])) {
       array_push($errors, "氏名を入力してください。");
+    }
+    if ((!empty($request['your_name'])) && 20 < mb_strlen($request['your_name'])) {
+      array_push($errors, "氏名は20文字以内で入力してください。");
+    }
+
+    // e-mailバリデーション
+    if (empty($request['your_email'])) {
+      array_push($errors, "メールアドレスを入力してください。");
+    }
+    if ((!empty($request['your_email'])) && filter_var($request['your_email'], FILTER_VALIDATE_EMAIL)) {
+      array_push($errors, "メールアドレスを正しい形式で入力してください。");
     }
 
     return $errors;
