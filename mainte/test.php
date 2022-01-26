37fd0e4925e13a3b3ff6bdd3ee06e931eb07fbfe
NaoyaTsudome <noy.dmt1021@gmail.com>
2022-01-26 23:32:54 +0900

    ファイル中身を行ごとに分割。行をさらに区切り文字で分割して配列に格納

--- mainte/test.php
+++ mainte/test.php
@@ -4,13 +4,29 @@
   // ファイルの中身丸ごと読み込んで変数に格納
   $fileContents = file_get_contents($contactFile);
 
-  echo $fileContents; // => あああ いいい ううう と画面上に表示
+  // echo $fileContents; // => あああ いいい ううう と画面上に表示
 
   // FILE_APPEND を引数にすることで末尾に テストです!!!!! を追記
   // FILE_APPEND を引数にしない場合、ファイル中身を全部書き換えする
-  file_put_contents($contactFile, "テストです！！！！！", FILE_APPEND);
+  // file_put_contents($contactFile, "テストです！！！！！", FILE_APPEND);
 
-  $fileContents = file_get_contents($contactFile); // => あああ いいい ううう テストです！！！！！　と画面上に表示
+  // $fileContents = file_get_contents($contactFile); // => あああ いいい ううう テストです！！！！！　と画面上に表示
 
-  echo $fileContents;
+  $allData = file($contactFile);
+  // echo '<pre>';
+  // var_dump($allData);
+  // echo '</pre>';
+  foreach($allData as $lineData) {
+    // echo '<pre>';
+    // var_dump($lineData);
+    // echo '</pre>';
+    $lines = explode(',', $lineData);
+    // echo '<pre>';
+    // var_dump($lines);
+    // echo '</pre>';
+
+    echo $lines[0] . ' ' . $lines[1] . ' ' . $lines[2] . ' ' . $lines[3];
+    echo '<br>';
+  }
+
 ?>
