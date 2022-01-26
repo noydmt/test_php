ba1130d9f1103cab949794e9bee2d704801692d7
NaoyaTsudome <noy.dmt1021@gmail.com>
2022-01-26 22:31:56 +0900

    コメントを微修正

--- mainte/test.php
+++ mainte/test.php
@@ -1,7 +1,7 @@
 <?php
   $contactFile = '.contact.dat';
 
-  // ファイルの中身ごと変数に格納
+  // ファイルの中身丸ごと読み込んで変数に格納
   $fileContents = file_get_contents($contactFile);
 
   echo $fileContents; // => あああ いいい ううう と画面上に表示
