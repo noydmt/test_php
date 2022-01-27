348bfc2b89d54596d9ce05169789c5fe0465d6f0
NaoyaTsudome <noy.dmt1021@gmail.com>
2022-01-27 23:19:34 +0900

    接続失敗した場合に処理を中断する

--- mainte/db_connection.php
+++ mainte/db_connection.php
@@ -1,8 +1,15 @@
 <?php
 
-  const DB_HOST = 'mysql:dname=udemy_php;host=127.0.0.1;charset=utf8;';
+  const DB_HOST = 'mysql:dbname=udemy_php;host=localhost;charset=utf8;';
   const DB_USER = 'php_user';
   const DB_PASSWORD = 'noydmt1021';
-  $pdo = new PDO(DB_HOST, DB_USER, DB_PASSWORD);
 
+  // DB接続ができていない場合の例外処理を行う
+  try {
+    $pdo = new PDO(DB_HOST, DB_USER, DB_PASSWORD);
+    echo '接続成功';
+  } catch (PDOException $e) {
+    echo $e->getMessage();
+    exit(); // 処理を抜ける。
+  }
 ?>
