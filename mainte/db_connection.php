727d6e14b043f1e95278c9c06342cc077f478170
NaoyaTsudome <noy.dmt1021@gmail.com>
2022-01-27 22:52:41 +0900

    myadminで作ったDBに接続

---
+++ mainte/db_connection.php
@@ -0,0 +1,8 @@
+<?php
+
+  const DB_HOST = 'mysql:dname=udemy_php;host=127.0.0.1;charset=utf8;';
+  const DB_USER = 'php_user';
+  const DB_PASSWORD = 'noydmt1021';
+  $pdo = new PDO(DB_HOST, DB_USER, DB_PASSWORD);
+
+?>
