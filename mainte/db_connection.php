78189b4231b4a73a450d617e7af28a5f1d2b8b93
NaoyaTsudome <noy.dmt1021@gmail.com>
2022-01-27 23:32:55 +0900

    DB接続時にオプションを追加

--- mainte/db_connection.php
+++ mainte/db_connection.php
@@ -6,7 +6,11 @@
 
   // DB接続ができていない場合の例外処理を行う
   try {
-    $pdo = new PDO(DB_HOST, DB_USER, DB_PASSWORD);
+    $pdo = new PDO(DB_HOST, DB_USER, DB_PASSWORD, [
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // => DBからの取得結果を連想配列で返す
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // => 例外を表示
+      PDO::ATTR_EMULATE_PREPARES => false, // => SQLインジェクション対策
+    ]);
     echo '接続成功';
   } catch (PDOException $e) {
     echo $e->getMessage();
