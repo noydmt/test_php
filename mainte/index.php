df17ec52920a8a8c5f5700344f654811113806bb
NaoyaTsudome <noy.dmt1021@gmail.com>
2022-01-28 21:43:32 +0900

    動的なSQLの作成

--- mainte/index.php
+++ mainte/index.php
@@ -2,13 +2,22 @@
   require 'db_connection.php';
 
   // 静的SQLの場合、query
-  $sql = 'select * from contacts;';
-  $stmt = $pdo->query($sql);
+  // $sql = 'select * from contacts;';
+  // $stmt = $pdo->query($sql);
+  // $result = $stmt->fetchall();
+
+  // echo '<pre>';
+  // var_dump($result);
+  // echo '</pre>';
+
+  // 動的SQLの場合、prepare, bind, execute
+  $sql = 'select * from contacts where id = :id;';
+  $stmt = $pdo->prepare($sql);
+  $stmt->bindValue('id', 1, PDO::PARAM_INT);
+  $stmt->execute();
   $result = $stmt->fetchall();
 
   echo '<pre>';
   var_dump($result);
   echo '</pre>';
-
-  // 動的SQLの場合、prepare, bind, excute
 ?>
