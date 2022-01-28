cabdd3dc7109a2ee898d95f4a2a6cf3027271bcb
NaoyaTsudome <noy.dmt1021@gmail.com>
2022-01-28 22:10:34 +0900

    トランザクション処理

--- mainte/index.php
+++ mainte/index.php
@@ -11,13 +11,33 @@
   // echo '</pre>';
 
   // 動的SQLの場合、prepare, bind, execute
-  $sql = 'select * from contacts where id = :id;';
-  $stmt = $pdo->prepare($sql);
-  $stmt->bindValue('id', 1, PDO::PARAM_INT);
-  $stmt->execute();
-  $result = $stmt->fetchall();
+  // $sql = 'select * from contacts where id = :id;';
+  // $stmt = $pdo->prepare($sql);
+  // $stmt->bindValue('id', 1, PDO::PARAM_INT);
+  // $stmt->execute();
+  // $result = $stmt->fetchall();
 
-  echo '<pre>';
-  var_dump($result);
-  echo '</pre>';
+  // echo '<pre>';
+  // var_dump($result);
+  // echo '</pre>';
+
+  try {
+    $pdo->beginTransaction(); // => トランザクション開始
+
+    // SQL処理
+    $sql = 'select * from contacts where id = :id;';
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindValue('id', 1, PDO::PARAM_INT);
+    $stmt->execute();
+
+    $pdo->commit(); // => トランザクション終了
+    
+    $result = $stmt->fetchall();
+
+    echo '<pre>';
+    var_dump($result);
+    echo '</pre>';
+  } catch (PDOException $e) {
+    $pdo->rollback();
+  }
 ?>
