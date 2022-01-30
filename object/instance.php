fa51fca3ee52d350c5e3b496c917f60190020666
NaoyaTsudome <noy.dmt1021@gmail.com>
2022-01-30 09:45:42 +0900

    クラス作成とインスタンス初期化、基本的な状態操作

---
+++ object/instance.php
@@ -0,0 +1,36 @@
+<?php
+  class Product {
+    private $product = [];
+
+    // コンストラクター
+    function __construct($product) {
+      $this->product = $product;
+    }
+
+    // 値取得
+    public function getProduct() {
+      echo $this->product;
+    }
+
+    // productに追加
+    public function addProduct($item) {
+      $this->product .= $item;
+    }
+
+    // static メソッド
+    public static function getStaticProduct($str) {
+      echo $str;
+    }
+  }
+
+  $instance = new Product('テスト');
+
+  $instance->getProduct(); // テスト
+  echo '<br>';
+
+  $instance->addProduct('追加分');
+  $instance->getProduct(); // テスト追加分
+  echo '<br>';
+
+  Product::getStaticProduct('静的メソッド'); // 静的メソッド
+?>
